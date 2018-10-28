31d3baab3ca3e5ecd6075f7b8f56a8234b078ded
Charles Stover <github@charlesstover.com>
2018-10-27 19:52:58 -0500

    React 16.7 prep

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,27 +1,28 @@
-import React from 'reactn';
+import React from 'react';
+import { useGlobal } from 'reactn';
 import AddCard from '../add-card/add-card';
 import Card from '../card/card';
 import './cards.scss';
 
 // Render all cards in the global state, plus an additional Add Card button.
-export default class Cards extends React.PureComponent {
+function Cards() {
+
+  console.log('before useGlobal');
+  const [ cards ] = useGlobal('cards');
+  console.log('after useGlobal');
 
   // For each card in the global state, render a Card component.
-  get cards() {
-    return this.global.cards.map(card =>
-      <Card
-        key={card.id}
-        {...card}
-      />
-    );
-  }
+  return (
+    <div className="cards">
+      {cards.map(card =>
+        <Card
+          key={card.id}
+          {...card}
+        />
+      )}
+      <AddCard />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="cards">
-        {this.cards}
-        <AddCard />
-      </div>
-    );
-  }
-}
+export default Cards;
