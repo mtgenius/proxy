e31dc1c20bdcbc01eb6382585fffbc3a03f15ba6
Charles Stover (via Travis CI) <github@charlesstover.com>
2020-03-05 01:53:28 +0000

    Deploy mtgenius/proxy to github.com/mtgenius/proxy.git:gh-pages

---
+++ precache-manifest.29f24a37aaffc8356348059b7d1cc1c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57d664f853798ddaf3939ac89c168b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81db9359ba34b089e018",
+    "url": "./static/css/main.e8f42024.chunk.css"
+  },
+  {
+    "revision": "c6f3737db7e3bb34369c",
+    "url": "./static/js/2.e8c8a59b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.e8c8a59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81db9359ba34b089e018",
+    "url": "./static/js/main.5814305b.chunk.js"
+  },
+  {
+    "revision": "af8a4396fd3e62f5309c",
+    "url": "./static/js/runtime-main.e8c62ef9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30220677c737a8672345c0b12b326b70.js"
+  "./precache-manifest.29f24a37aaffc8356348059b7d1cc1c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
