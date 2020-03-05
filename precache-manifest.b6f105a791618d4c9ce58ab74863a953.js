0910eacee90b5dad796e34f18506f0edd8a31b1b
Charles Stover (via Travis CI) <github@charlesstover.com>
2020-03-05 01:51:01 +0000

    Deploy mtgenius/proxy to github.com/mtgenius/proxy.git:gh-pages

---
+++ precache-manifest.b6f105a791618d4c9ce58ab74863a953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f1e5c80caedde356b14052d61cc6ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c8905f136fb488ebd6",
+    "url": "/static/css/main.e8f42024.chunk.css"
+  },
+  {
+    "revision": "c6f3737db7e3bb34369c",
+    "url": "/static/js/2.e8c8a59b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e8c8a59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c8905f136fb488ebd6",
+    "url": "/static/js/main.bfc9b562.chunk.js"
+  },
+  {
+    "revision": "1e33ea82572b1b910ebe",
+    "url": "/static/js/runtime-main.28de64a2.js"
+  }
+]);
